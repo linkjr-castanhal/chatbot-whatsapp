1c985dfb255126b19f69c3ce7f55ddafc3bd79a8
ItaloSSilva <silva_italo190@hotmail.com>
2024-12-04 21:22:10 -0300

    feat:init project

---
+++ index.js
@@ -0,0 +1,24 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client, LocalAuth } = require('whatsapp-web.js');
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('message_create', message => {
+	if (message.body === '!ping') {
+		client.sendMessage(message.from, 'pong');
+	}
+});
+
+client.initialize();
