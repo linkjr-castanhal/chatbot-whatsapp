dce47d1c9873ba5acd56c44816e417853b4a6c63
Italo Souza Silva <145210705+ItaloSSilva@users.noreply.github.com>
2024-12-07 15:47:41 -0300

    Update index.js
    Atualizado linha 41:
    Consertei para que o código enviasse a mensagem aos sábados e não nas segundas.

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function fimDeSemana() {
     let strtres = '';
     if (dia === 0) { // Domingo
         strtres = '🏖️ *Aproveite o domingo!*\n\n😃 Entraremos em contato assim que possível.\n\n🕘 Nosso horário é de segunda a sexta de 09:00hs às 19:00hs\n\n*Atendimento presencial mediante agendamento.*';
-    } else if (dia === 1) { // Sábado
+    } else if (dia === 6) { // Sábado
         strtres = '🏖️ *Aproveite o sábado!*\n\n😃 Entraremos em contato assim que possível.\n\n🕘 Nosso horário é de segunda a sexta de 09:00hs às 19:00hs\n\n*Atendimento presencial mediante agendamento.*';
     }
     return strtres;
